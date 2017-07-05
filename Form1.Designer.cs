c962d260c202cb0adc9bdfce1a247b4ece4b39d9
GV_Yich <yich.lin@Gravity.co.kr>
2017-07-05 18:15:20 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ChangeVersionNumberTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.ndVersionNumber = new System.Windows.Forms.NumericUpDown();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ndVersionNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "請輸入版本號";
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Enabled = false;
+            this.tbFolderPath.Location = new System.Drawing.Point(14, 24);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.Size = new System.Drawing.Size(200, 22);
+            this.tbFolderPath.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "專案路徑";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(220, 22);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "瀏覽";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(220, 51);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 52);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // ndVersionNumber
+            // 
+            this.ndVersionNumber.Location = new System.Drawing.Point(94, 54);
+            this.ndVersionNumber.Maximum = new decimal(new int[] {
+            1241513983,
+            370409800,
+            542101,
+            0});
+            this.ndVersionNumber.Name = "ndVersionNumber";
+            this.ndVersionNumber.Size = new System.Drawing.Size(120, 22);
+            this.ndVersionNumber.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "進度:";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(45, 90);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(41, 12);
+            this.lbStatus.TabIndex = 8;
+            this.lbStatus.Text = "未執行";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(219, 36);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "說明:\r\n匹配路徑下所有的html檔，將\r\n\"?version=數字\" 的字串換成指定的版本號\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 167);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ndVersionNumber);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbFolderPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "版本號變更工具";
+            ((System.ComponentModel.ISupportInitialize)(this.ndVersionNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown ndVersionNumber;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeVersionNumberTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //tbFolderPath.Text = Properties.Settings.Default.FolderPath;
+            tbFolderPath.Text = "C:\\Users\\user\\Desktop\\20170623_EP16.1update";
+
+        }
+
+        //瀏覽按鈕
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbFolderPath.Text = folderBrowserDialog1.SelectedPath;
+                Properties.Settings.Default["FolderPath"] = folderBrowserDialog1.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        //開始按鈕
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //取得此資料夾內 所有副檔名為html的檔案
+            string[] files = Directory.GetFiles(tbFolderPath.Text, "*.html", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                //讀取檔案的內容
+                string html = File.ReadAllText(file);
+                //將version number取代掉
+                string newHtml = Regex.Replace(html, @"\?version=(\d*)", "?version="+ ndVersionNumber.Value.ToString());
+                //寫回檔案
+                File.WriteAllText(file, newHtml);
+            }
+
+            lbStatus.Text = "成功";
+
+        }
+    }
+}
